582ec4162e52b3038cec03c1d4f0026883a9bb11
KatherineQuintana <katty.quintana@gmail.com>
2021-09-15 09:15:11 -0300

    cambios finales

--- src/cipher.js
+++ src/cipher.js
@@ -1,5 +1,38 @@
 const cipher = {
-  // ...
+  
+ encode: (write, password)=> {
+   if (password === "null" || password === 0) {
+     throw new TypeError(alert("ingrese un código válido"));     
+   }
+    let mensEncod = "";
+    for (let i = 0; i < write.length; i++) {
+      let asciiNum = write.charCodeAt(i); //letra transformada a N°ASCII
+      //considero ASCII desde el 32 para tomar los signos por lo que el residuo es 58% por los caracteres incluidos
+      let positionNew = (asciiNum - 32 + password) %58 + 32;
+      mensEncod += String.fromCharCode(positionNew);
+    }
+    return mensEncod;
+  },
+
+  decode: (writeDos, passwordDos)=>{
+    if (passwordDos === "null" || passwordDos === 0) {
+      throw new TypeError(alert("ingrese un código válido"));     
+    }
+    let mensDecod="";
+    for (let i = 0; i < writeDos.length; i++) {
+      let asciiNumDos = writeDos.charCodeAt(i);
+      let positive = asciiNumDos - 32 - passwordDos;
+     // obliga a que el valor sea siempre positvo
+      while (positive<0) {
+        positive = positive + 58;        
+      }
+      let positionNewDos = positive % 58 + 32;   
+      mensDecod += String.fromCharCode(positionNewDos);   
+    }
+    return mensDecod;
+  },
 };
 
 export default cipher;
+
+
--- src/index.js
+++ src/index.js
@@ -1,3 +1,70 @@
 import cipher from './cipher.js';
 
 console.log(cipher);
+
+//funcion para botones
+
+    document.getElementById("encode").addEventListener("click", (e) => {
+        e.preventDefault()
+        document.getElementById("root").style.display="none";
+        document.getElementById("pageTwo").style.display="block";
+    });
+
+    document.getElementById("decode").addEventListener("click", (e) => {
+        e.preventDefault()
+        document.getElementById("pageThree").style.display="block";
+        document.getElementById("pageTwo").style.display="none";
+        document.getElementById("root").style.display="none"
+    });
+
+    document.getElementById("returnHome").addEventListener("click", ()=>{
+        document.getElementById("root").style.display="block";
+        document.getElementById("pageTwo").style.display="none";
+    });
+
+    document.getElementById("pageRoot").addEventListener("click", ()=>{
+        document.getElementById("pageThree").style.display="none";
+        document.getElementById("root").style.display="block";
+    });
+
+    document.getElementById("clean").addEventListener("click", ()=> {
+        write.value = "";
+        result.value = "";
+        password.value = "";
+    });
+
+    document.getElementById("cleanDos").addEventListener("click", ()=> {
+        writeDos.value = "";
+        resultDos.value = "";
+        passwordDos.value = "";
+    });
+
+//función de cipher//
+
+//llamar write para que quede en mayúscula//
+//que tome el valor de write//
+//que tome el valor de password//
+
+//página dos
+
+    document.getElementById("write").addEventListener("keyup", ()=> {
+        write.value = write.value.toUpperCase();        
+    });
+
+    document.getElementById("send").addEventListener("click", ()=> {
+        let write = document.getElementById("write").value;
+        let password = parseInt(document.getElementById("password").value);
+        document.getElementById("result").value = cipher.encode(write, password);
+    });
+     
+//página tres
+
+    document.getElementById("writeDos").addEventListener("keyup", ()=> {
+        writeDos.value = writeDos.value.toUpperCase();        
+    });
+
+     document.getElementById("sendDos").addEventListener("click", ()=> {
+        let writeDos = document.getElementById("writeDos").value;
+        let passwordDos = parseInt(document.getElementById("passwordDos").value);
+        document.getElementById("resultDos").value = cipher.decode(writeDos, passwordDos);
+   });
